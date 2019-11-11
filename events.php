00de796b8deea9cc7397ab02c23c6e21678500b8
azronbrian <azroneo@gmail.com>
2019-11-11 16:57:26 +0300

    Events page

---
+++ events.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style media="screen">
+		img{
+			display: block;
+			margin-left: auto;
+			margin-right: auto;
+		}
+	</style>
+</head>
+<body>
+	<div>
+		<img src="selesta19.jpg" alt="selesta">
+	</div>
+</body>
+</html>
