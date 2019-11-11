fa459962c3ca1fe4153ac90d121d833eee914600
azronbrian <azroneo@gmail.com>
2019-11-11 16:41:06 +0300

    templates

---
+++ template/footer.php
@@ -0,0 +1,8 @@
+           <div id="footer">
+           	  <a href="team.php"><h2>&copy; 2019 - Development Team</h2></a>
+           </div>
+	</div>
+	<!--container ends-->
+
+</body>
+</html>
---
+++ template/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>SVCET Alumni</title>
+	<link rel="icon" type="image/ico" href="logo.jpg">
+	<meta name="viewport" content="width-device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="styles/style.css" media="all">
+</head>
+<body>
+	<!--container starts-->
+	<div class="container">
+		<!--header wrap starts-->
+		<div id="head_wrap">
+			<!--header  starts-->
+			<div id="header">
+				<img src="logo.jpg">
+				<h2 style="float:left ;color: white">SVCET ALUMNI ASSOCIATION PORTAL</h2>
+				<form method="post" id="form1">
+					<input type="email" name="email" placeholder="Email" required="required">
+					<input type="password" name="pass" placeholder="password" required="required">
+					<button name="login">Login</button>
+
+				</form>
+
+			</div>
+			<!--header ends-->
+		</div>
+           <!--header wrap ends-->
+					 <div class="nav">
+            	<a href="http://svcetedu.org/wp/about-svcet/">About SVCET</a>
+            	<a href="gallery.php">Gallery</a>
+            	<a href="events.php">Events</a>
+            	<a href="team.php">Contact us</a>
+            </div>
